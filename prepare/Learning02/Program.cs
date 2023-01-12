df05f0f0a95fac87886add5cbed7055a7d1986f8
jordancarron11 <jordancarron11@gmail.com>
2023-01-12 09:11:09 -0700

    add

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        string letter = "";
+        Console.WriteLine("What grade did you get in the class? ");
+        string number = Console.ReadLine();
+        int grade = int.Parse(number);
+
+        if (grade >= 90){
+            letter = ("A");
+        }
+
+        else if (grade >=80 ){
+            letter = ("B");
+        }
+        
+        else if (grade >=70 ){
+            letter = ("C");
+        }
+        else if (grade >=60 ){
+            letter = ("D");
+        }
+        else{
+            letter = ("F");
+        }
+        
+        Console.Write("your grade is: ");
+        Console.WriteLine(letter);
+
+
+        if (grade >= 70){
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else{
+            Console.WriteLine("Unfortunately you failed. Good luck next time!");
+        }
+        
     }
+
+   
 }
