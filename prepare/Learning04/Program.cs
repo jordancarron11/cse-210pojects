ab16b25696a4fda08654dd43a8308bcc03859784
jordancarron11 <jordancarron11@gmail.com>
2023-01-12 12:40:06 -0700

    add

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+class Program{
+    static void Main(string[] args){
+        List<int> ListOfNumbers = new List<int>();
+        
+        int userNumber = -1;
+        while (userNumber != 0){
+            Console.Write("Enter a number: type 0 to quit ");
+            
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+            
+            if (userNumber != 0){
+                ListOfNumbers.Add(userNumber);
+            }
+        }
+        // Sum 
+        int SumOfList = 0;
+        foreach (int number in ListOfNumbers)
+        {
+            SumOfList += number;
+        }
+
+        Console.WriteLine($"Sum: {SumOfList}");
+        // Average
+        float average = ((float)SumOfList) / ListOfNumbers.Count;
+        Console.WriteLine($"Avg: {average}");
+        
+        int maxNum = ListOfNumbers[0];
+
+        //Max
+        foreach (int number in ListOfNumbers){
+            if (number > maxNum){
+                maxNum = number;
+            }
+        }
+
+        Console.WriteLine($"Max: {maxNum}");
     }
 }
