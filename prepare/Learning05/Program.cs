ad31bec3fcfd87c1ee17c1f9178251092f5092cb
jordancarron11 <jordancarron11@gmail.com>
2023-01-12 18:15:14 -0700

    add

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        WelcomMessage();
+        
+        string name = GetUserName();
+        int number = UserNumber();
+
+        int squarednum = SquaredNumber(number);
+
+        Result(name, squarednum);
     }
+// Welcome message function
+    static void WelcomMessage(){
+        Console.WriteLine("Welcome to the Squared Number Calculator!");
+    }
+
+//Username function
+    static string GetUserName(){
+        Console.Write("Please enter your UserName: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+// users Integer function 
+static int UserNumber(){
+    Console.Write("Enter a number: ");
+    int userNum = int.Parse(Console.ReadLine());
+
+    return userNum;
+}
+// Converts Int to the number ^2
+static int SquaredNumber(int number){
+    int square = number * number;
+    
+    return square;
+}
+static void Result (string name, int number){
+    Console.WriteLine($"{name} your number squared is: {number}");
+}
+
+
 }
