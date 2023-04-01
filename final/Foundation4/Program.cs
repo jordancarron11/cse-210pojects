ec618c59f482683487563e8bad9bab8d001aa8bd
jordancarron11 <jordancarron11@gmail.com>
2023-03-31 22:24:49 -0600

    Finished foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activitiesList = new List<Activity>();
+
+        Running running = new Running(20, 2.5);
+        activitiesList.Add(running);
+        Cycling cycling = new Cycling(60, 10);
+        activitiesList.Add(cycling);
+        Swimming swimming = new Swimming(30, 20);
+        activitiesList.Add(swimming);
+
+
+        //display activity summaries
+        foreach (Activity activity in activitiesList)
+        {
+            activity.DisplaySummary();
+        }
     }
 }
